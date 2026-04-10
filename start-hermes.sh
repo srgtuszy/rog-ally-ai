31a542af630dba9d048f9780a2f6aed8e5adc1c5
Michal Tuszynski <srgtuszy@gmail.com>
2026-04-10 22:46:01 +0200

    Add Hermes 3 tool calling script (128K context)

---
+++ start-hermes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Usage: ./start-hermes.sh [context_size] [port]
+# Default: 262144 context (256K), port 8002
+
+cd /var/home/srgtuszy/gpu-setup
+export ROCR_VISIBLE_DEVICES=1
+export LD_LIBRARY_PATH=bin:rocm7-libs:$LD_LIBRARY_PATH
+
+CTX="${1:-262144}"
+PORT="${2:-8002}"
+
+exec ./llama-server-rocm2 \
+    -m data/models/Hermes-3-8B.Q4_K_M.gguf \
+    -ngl 99 \
+    -c "${CTX}" \
+    -ctk q4_0 \
+    -ctv q4_0 \
+    --port "${PORT}" \
+    --jinja \
+    --no-warmup
